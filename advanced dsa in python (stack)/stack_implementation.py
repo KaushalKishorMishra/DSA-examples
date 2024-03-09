cf3378f6bc8f4f1d87dce00473e7fe52c8f62da7
KaushalKishorMishra <077bcsit016.kaushal@scst.edu.np>
2024-03-09 15:05:08 +0545

    add: stack implementation code in python

---
+++ advanced dsa in python (stack)/stack_implementation.py
@@ -0,0 +1,70 @@
+# stack implementation
+class Stack:
+    # constructor for the stack class
+    def __init__(self):
+        self.stack = []
+
+    # method to push elements to the stack
+    def push(self, data):
+        self.stack.append(data)
+
+    # method to check wether the stack is empty or not
+    def is_empty(self):
+        return len(self.stack) == 0
+
+    # method to get the size of the stack
+    def size(self):
+        return len(self.stack)
+
+    # method to pop elements from the stack
+    def pop(self):
+        if self.is_empty():
+            return "Stack is empty"
+        return self.stack.pop()
+
+    # method to get the top element of the stack
+    def peek(self):
+        if self.is_empty():
+            return "Stack is empty"
+        return self.stack[-1]
+
+    # method to print the stack
+    def __str__(self):
+        return str(self.stack)
+
+
+# create a stack object
+stack = Stack()
+# checking the size of the stack
+print(f"Stack is of size: {stack.size()}")
+# pushing elements to the stack
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.push(4)
+stack.push(5)
+# checking wether the stack is empty or not
+print(f"Stack is empty: {stack.is_empty()}")
+# checking the size of the stack
+print(f"Stack is of size: {stack.size()}")
+# checking the top element of the stack
+print(f"Stack peek: {stack.peek()}")
+# printing the stack
+print(f"Stack: {stack}")
+# popping elements from the stack
+stack.pop()
+print(f"Stack is of size: {stack.size()}")
+print(f"Stack peek: {stack.peek()}")
+print(f"Stack: {stack}")
+stack.pop()
+print(f"Stack is of size: {stack.size()}")
+print(f"Stack peek: {stack.peek()}")
+print(f"Stack: {stack}")
+stack.pop()
+print(f"Stack is of size: {stack.size()}")
+print(f"Stack peek: {stack.peek()}")
+print(f"Stack: {stack}")
+stack.pop()
+print(f"Stack is of size: {stack.size()}")
+print(f"Stack peek: {stack.peek()}")
+print(f"Stack: {stack}")
